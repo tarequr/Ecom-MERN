9c7c236c71ed8b210e83cbb4410f8feb3b202ee3
tarequr <tarequrr8@gmail.com>
2024-09-21 20:31:49 +0600

    Core: user searching and pagination

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -1,11 +1,38 @@
 const createError = require('http-errors');
-// const users = require('../models/userModel');
+const User = require('../models/userModel');
 
-const getUsers = (req, res, next) => {
+const getUsers = async (req, res, next) => {
     try {
+        const search = req.query.search || "";
+        const page   = Number(req.query.page) || 1;
+        const limit  = Number(req.query.limit) || 5;
+
+        const searchRegex = new RegExp('.*' + search + '.*', 'i');  // here 'i' means case insensitive
+        const filter = {
+            isAdmin: {$ne: true},    //The $ne operator in MongoDB stands for "not equal"
+            $or: [
+                { name: { $regex: searchRegex } },    // Match if 'name' field matches regex pattern
+                { email: { $regex: searchRegex } },   // Match if 'email' field matches regex pattern
+                { phone: { $regex: searchRegex } },   // Match if 'phone' field matches regex pattern
+            ]
+        }
+        const options = { password: 0 }
+
+        const users = await User.find(filter, options).limit(limit).skip((page -1) * limit);
+        const count = await User.find(filter).countDocuments();
+
+        if (!users) throw createError(404, 'User not found');
+
         res.status(200).send({
             message: 'All user data',
-            // users
+            totalCount: users.length,
+            users,
+            pagination: {
+                totalPages: Math.ceil(count / limit),
+                currentPage: page,
+                previousPage: page - 1 > 0 ? page - 1 : null,
+                nextPage: page + 1 <= Math.ceil(count / limit) ? page + 1 : null
+            }
         });
     } catch (error) {
         next(error);
