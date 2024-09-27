333f6ef8527dc0a859e341953835ba5b91473775
tarequr <tarequrr8@gmail.com>
2024-09-27 23:42:29 +0600

    core: fetch secret key

--- backend/src/secret.js
+++ backend/src/secret.js
@@ -2,5 +2,6 @@ require('dotenv').config();
 const serverPort = process.env.PORT || 8080;
 const mongodbURL = process.env.MONGODB_URL || "mongodb://localhost:27017/ecommerce";
 const defaultImagePath = process.env.DEFAULT_IMAGE || "public/images/default.png";
+const jwtActivationKey = process.env.JWT_ACTIVATION_KEY || "jwtActivationKey";
 
-module.exports = { serverPort, mongodbURL, defaultImagePath }
+module.exports = { serverPort, mongodbURL, defaultImagePath, jwtActivationKey }
