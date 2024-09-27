2849a007afb324b83f4fb58585147833fbbb3f68
tarequr <tarequrr8@gmail.com>
2024-09-28 00:02:19 +0600

    core: json token refactoring

--- backend/src/helpers/jsonwebtoken.js
+++ backend/src/helpers/jsonwebtoken.js
@@ -1,8 +1,21 @@
 const jwt = require('jsonwebtoken');
 
 const createJSONWebToken = (payload, secretKey, expiresIn) => {
-    const token = jwt.sign(payload, secretKey, { expiresIn });
-    return token;
+    if (!payload || typeof payload != 'object') {
+       throw new Error('Payload must be a non-empty object'); 
+    }
+
+    if (secretKey ==  '' || typeof secretKey !== 'string') {
+        throw new Error('Secret key must be a non-empty string'); 
+    }
+
+    try {
+        const token = jwt.sign(payload, secretKey, { expiresIn });
+        return token;
+    } catch (error) {
+        console.error('Failed to sign the JWT', error);
+        throw error;
+    }
 }
 
 module.exports = { createJSONWebToken }
