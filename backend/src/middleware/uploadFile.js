56f793d6aabbdc54a3b1dd8528312ed0d105fb18
tarequr <tarequrr8@gmail.com>
2024-09-29 22:56:32 +0600

    core: file upload code refactor

--- backend/src/middleware/uploadFile.js
+++ backend/src/middleware/uploadFile.js
@@ -16,7 +16,7 @@ const storage = multer.diskStorage({
         // const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
         // cb(null, file.fieldname + '-' + uniqueSuffix)
         const extname = path.extname(file.originalname);
-        cb(null, Date.now() + '-' + file.originalname);
+        cb(null, Date.now() + '-' + file.originalname.replace(extname, '') + extname);
     }
 })
 
