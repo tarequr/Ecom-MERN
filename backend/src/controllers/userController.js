7a8c77b27185d622650403e0d9c5616f9b3f3f91
tarequr <tarequrr8@gmail.com>
2024-10-23 20:57:12 +0600

    core: service for finding all users

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -10,7 +10,7 @@ const { deleteImage } = require('../helpers/deleteImage');
 const { createJSONWebToken } = require('../helpers/jsonwebtoken');
 const { jwtActivationKey, clientURL } = require('../secret');
 const emailWithNodeMailer = require('../helpers/email');
-const { hadleUserAction } = require('../services/userService');
+const { hadleUserAction, findUsers } = require('../services/userService');
 
 
 const processRegister = async (req, res, next) => {
@@ -127,34 +127,8 @@ const getUsers = async (req, res, next) => {
         const page   = Number(req.query.page) || 1;
         const limit  = Number(req.query.limit) || 5;
 
-        const searchRegex = new RegExp('.*' + search + '.*', 'i');  // here 'i' means case insensitive
-        const filter = {
-            isAdmin: {$ne: true},    //The $ne operator in MongoDB stands for "not equal"
-            $or: [
-                { name: { $regex: searchRegex } },    // Match if 'name' field matches regex pattern
-                { email: { $regex: searchRegex } },   // Match if 'email' field matches regex pattern
-                { phone: { $regex: searchRegex } },   // Match if 'phone' field matches regex pattern
-            ]
-        }
-        const options = { password: 0 }
-
-        const users = await User.find(filter, options).limit(limit).skip((page -1) * limit);
-        const count = await User.find(filter).countDocuments();
-
-        if (!users) throw createError(404, 'User not found');
-
-        // res.status(200).send({
-        //     message: 'All user data',
-        //     totalCount: users.length,
-        //     users,
-        //     pagination: {
-        //         totalPages: Math.ceil(count / limit),
-        //         currentPage: page,
-        //         previousPage: page - 1 > 0 ? page - 1 : null,
-        //         nextPage: page + 1 <= Math.ceil(count / limit) ? page + 1 : null
-        //     }
-        // });
-
+        const { users, count } = await findUsers(search, limit, page);
+        
         return successResponse(res, {
             statusCode: 200,
             message: 'Users data fatch successfully',
--- backend/src/services/userService.js
+++ backend/src/services/userService.js
@@ -1,6 +1,42 @@
 const createError = require("http-errors");
 const User = require("../models/userModel");
 
+const findUsers = async (search, limit, page) => {
+    try {
+        const searchRegex = new RegExp('.*' + search + '.*', 'i');  // here 'i' means case insensitive
+        const filter = {
+            isAdmin: {$ne: true},    //The $ne operator in MongoDB stands for "not equal"
+            $or: [
+                { name: { $regex: searchRegex } },    // Match if 'name' field matches regex pattern
+                { email: { $regex: searchRegex } },   // Match if 'email' field matches regex pattern
+                { phone: { $regex: searchRegex } },   // Match if 'phone' field matches regex pattern
+            ]
+        }
+        const options = { password: 0 }
+
+        const users = await User.find(filter, options).limit(limit).skip((page -1) * limit);
+        const count = await User.find(filter).countDocuments();
+
+        if (!users) throw createError(404, 'User not found');
+
+        // res.status(200).send({
+        //     message: 'All user data',
+        //     totalCount: users.length,
+        //     users,
+        //     pagination: {
+        //         totalPages: Math.ceil(count / limit),
+        //         currentPage: page,
+        //         previousPage: page - 1 > 0 ? page - 1 : null,
+        //         nextPage: page + 1 <= Math.ceil(count / limit) ? page + 1 : null
+        //     }
+        // });
+
+        return { users, count };
+    } catch (error) {
+        throw (error);
+    }
+}
+
 const hadleUserAction = async (userId, action) => {
     try {
         let update;
@@ -30,4 +66,4 @@ const hadleUserAction = async (userId, action) => {
     }
 }
 
-module.exports = { hadleUserAction }
+module.exports = { findUsers, hadleUserAction }
