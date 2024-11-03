74e6428fed5ca9401fc98f408393ae3f5ce9bc62
tarequr <tarequrr8@gmail.com>
2024-11-03 20:40:35 +0600

    core: use product service for product create

--- backend/src/controllers/productController.js
+++ backend/src/controllers/productController.js
@@ -9,49 +9,10 @@ const handleCreateProduct = async (req, res, next) => {
     try {
         const { name, description, price, quantity, sold, shipping, category } = req.body;
 
-        // const image = req.file;
-
-        // if (!image) {
-        //     throw createError(400, 'Image field is required.')
-        // }
-
-        // if (image.size > 1024 * 1024 * 2) {
-        //     throw createError(400, 'File size must be between 2 MB')
-        // }
-
-        // const imageBufferString = image.buffer.toString('base64');
-
-        // anoher way
         const image = req.file?.path;
 
-        if (!image) {
-            throw createError(400, 'Image file is required');
-        }
-
-        if (image && image.size > 1024 * 1024 * 2) {
-            throw createError(400, 'File size must be between 2 MB');
-        }
-
-        const productExists = await Product.exists({ name: name });
-
-        if (productExists) {
-            throw createError(409, 'Product with this name already exists.');
-        }
-
-        // const newCategory = await createProduct(name);
-
-        //create product
-        const product = await Product.create({
-            name: name,
-            slug: slugify(name),
-            description: description,
-            price: price,
-            quantity: quantity,
-            sold: sold,
-            shipping: shipping,
-            image: image,
-            category: category
-        });
+        const productData = { name, description, price, quantity, sold, shipping, category, image };
+        const product = await createProduct(productData);
 
         return successResponse(res, {
             statusCode: 201,
--- backend/src/services/productService.js
+++ backend/src/services/productService.js
@@ -2,10 +2,37 @@ const slugify = require('slugify');
 const Product = require('../models/productModel');
 const createError = require('http-errors');
 
-const createProduct = async (req) => {
-    const { name } = req.body;
-    const newProduct = await Product.create({ name: name, slug: slugify(name) });
-    return newProduct;
+const createProduct = async (productData) => {
+    const { name, description, price, quantity, sold, shipping, category, image } = productData;
+
+    if (!image) {
+        throw createError(400, 'Image file is required');
+    }
+
+    if (image && image.size > 1024 * 1024 * 2) {
+        throw createError(400, 'File size must be between 2 MB');
+    }
+
+    const productExists = await Product.exists({ name: name });
+
+    if (productExists) {
+        throw createError(409, 'Product with this name already exists.');
+    }
+
+    //create product
+    const product = await Product.create({
+        name: name,
+        slug: slugify(name),
+        description: description,
+        price: price,
+        quantity: quantity,
+        sold: sold,
+        shipping: shipping,
+        image: image,
+        category: category
+    });
+
+    return product;
 }
 
 module.exports = { createProduct };
