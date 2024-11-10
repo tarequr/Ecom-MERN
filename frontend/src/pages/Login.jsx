ff288fac8f53a3b5255c9d8811c24bc8289ab998
tarequr <tarequrr8@gmail.com>
2024-11-10 22:46:27 +0600

    feat: create Home, Registe, Login, Cart, Error page

---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Login() {
+  return (
+    <div>
+        <h1>Login Page</h1>
+        <h2>Login form</h2>
+    </div>
+  )
+}
+
+export default Login
---
+++ frontend/src/pages/Register.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Register() {
+  return (
+    <div>
+        <h1>Register Page</h1>
+        <h2>Register form</h2>
+    </div>
+  )
+}
+
+export default Register
