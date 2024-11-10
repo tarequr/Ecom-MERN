d9335da0302ba40f445278ff039ddeeedc8f95c7
tarequr <tarequrr8@gmail.com>
2024-11-10 22:52:36 +0600

    feat: basic routing setup

--- frontend/src/routes/index.jsx
+++ frontend/src/routes/index.jsx
@@ -1,10 +1,19 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "../pages/Home";
+import Register from "../pages/Register";
+import Login from "../pages/Login";
+import Cart from "../pages/Cart";
+import Error from "../pages/Error";
 
 const  index = () => {
   return (
     <BrowserRouter>
         <Routes>
-            <Route/>
+            <Route path="/" element={<Home/>}/>
+            <Route path="/register" element={<Register/>}/>
+            <Route path="/login" element={<Login/>}/>
+            <Route path="/cart" element={<Cart/>}/>
+            <Route path="/*" element={<Error/>}/>
         </Routes>
     </BrowserRouter>
   )
