a07d818d64c08110724d41c3c3fd984554fa474b
tarequr <tarequrr8@gmail.com>
2024-11-13 22:38:41 +0600

    core: sidebar & home page setup

---
+++ frontend/src/components/ProductSidebar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function ProductSidebar() {
+    return (
+        <aside className='sidebar'>
+            <div>
+                <h3>Filter by Price</h3>
+            </div>
+            <div>
+                <h3>Filter by Categories</h3>
+            </div>
+        </aside>
+    )
+}
+
+export default ProductSidebar
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,14 +1,21 @@
 import React from 'react'
 import PageTitle from '../components/PageTitle'
+import ProductSidebar from '../components/ProductSidebar'
 
 
 function Home() {
   return (
-    <div>
+    <>
       <PageTitle title="Home" />
-      <h1>Home Page</h1>
-      <h2>List of all products</h2>
-    </div>
+      <div className='container flex-space-around'>
+        <div className='sidebar-container'>
+          <ProductSidebar />
+        </div>
+        <div className='main-container'>
+          <h2>List of All products</h2>
+        </div>
+      </div>
+    </>
   )
 }
 
